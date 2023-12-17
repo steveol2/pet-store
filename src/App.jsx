44b7e85c8320a436e5762f5fe10dc75f7119e549
steveol2 <stefanoletu@gmail.com>
2023-12-17 13:00:20 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <header className="bg-blue-300 font-bold p-2 text-white">
+        <a href="">Marley</a>
+      </header>
+      <main>list products</main>
+    </>
+  );
+}
+
+export default App;
