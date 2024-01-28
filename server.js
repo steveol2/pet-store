c8375727743752d6112f111fcbb21d43e91fb2ad
steveol2 <stefanoletu@gmail.com>
2024-01-28 20:02:31 +0100

    data updated

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
+// server.js
 import express from "express";
-import data from "./data.js";
+import products from "./data.js"; // Import the array directly
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
+
 app.get("/api/products", (req, res) => {
-  res.send(data.products);
+  console.log("Request received for /api/products");
+  console.log("Sending products data:", products);
+
+  // Set content type to application/json
+  res.setHeader("Content-Type", "application/json");
+
+  res.json({ products });
+});
+
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.setHeader("Content-Type", "application/json");
+    res.json(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
+  }
+});
+
+app.get("/api/products/_id/:id", (req, res) => {
+  const product = products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.setHeader("Content-Type", "application/json");
+    res.json(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
+  }
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
